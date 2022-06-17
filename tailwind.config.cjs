373870f1c4aa8027db0bc5ccde9e9a408f2e4b20
babakfp <44144724+babakfp@users.noreply.github.com>
2022-06-17 10:44:38 -0700

    Removed unneeded tw classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,16 +2,7 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte}'],
   theme: {
     extend: {
-      // maxWidth: theme => theme('spacing'),
       minWidth: theme => theme('spacing'),
-      minHeight: theme => theme('spacing'),
-      spacing: {
-        38: '9.5rem',
-        57: '14.25rem',
-      },
-      fontSize: {
-        '2xs': ['0.625rem', { lineHeight: '0.875rem' }],
-      },
       borderRadius: {
         DEFAULT: '8px',
       },
