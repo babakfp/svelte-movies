27e4d384b3351b9c48a4897b2e86cbbf80587eba
babakfp <44144724+babakfp@users.noreply.github.com>
2022-06-17 11:41:35 -0700

    Removed top rated movies from home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,14 +27,6 @@
   export let movies
   import MovieCard from '$lib/MovieCard.svelte'
   import MovieCardPlaceholder from '$lib/MovieCardPlaceholder.svelte'
-
-  // ---
-
-  const topRatedMovies = (async () => {
-		const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${import.meta.env.VITE_API_KEY}`)
-    const data = await res.json()
-    return data.results.slice(0, 14)
-	})()
 </script>
 
 <section class="container | grid gap-6">
@@ -56,37 +48,4 @@
       </li>
     {/each}
   </ul>
-</section>
-
-<section class="container | grid gap-6 | mt-12">
-  <div class="flex items-end justify-between">
-    <h2 class="text-xl font-semibold">Top Rated</h2>
-    <!-- <a class="text-sm text-gray-400 underline" href="/movies">See More</a> -->
-  </div>
-
-  {#await topRatedMovies}
-    <ul class="grid grid-cols-[repeat(auto-fill,minmax(9rem,1fr))] gap-6">
-      {#each [...Array(14).keys()]  as _}
-        <li class="flex justify-center">
-          <MovieCardPlaceholder />
-        </li>
-      {/each}
-    </ul>
-  {:then movies}
-    <ul class="grid grid-cols-[repeat(auto-fill,minmax(9rem,1fr))] gap-6">
-      {#each movies as movie}
-        <li class="flex justify-center">
-          <MovieCard
-            id={movie.id}
-            title={movie.title}
-            poster_path={movie.poster_path}
-            vote_average={movie.vote_average}
-            release_date={movie.release_date}
-          />
-        </li>
-      {/each}
-    </ul>
-  {:catch error}
-    <p>Something went wrong!</p>
-  {/await}
 </section>
